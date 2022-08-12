aaeba9c35c50a9ad225218f927750396fab8fa25
jeraldthatman <39328604+jeraldthatman@users.noreply.github.com>
2022-08-12 00:50:08 -0400

    initial

---
+++ functions.py
@@ -0,0 +1,51 @@
+def prep_df(df):
+
+    from sklearn import preprocessing
+    from collections import deque
+    
+    df = df.drop('future',axis=1)
+        # Shifting the target variable 3 days ahead
+
+    for col in df.columns:
+        if col != "target":
+            df[col] = df[col].pct_change() # gets the percent change 
+            df.dropna(inplace=True)
+            df[col] = preprocessing.scale(df[col].values)
+    df.dropna(inplace=True)
+
+    seq_data = []
+    prev_days = deque(maxlen = seq_len) # appends all the data to the empty list, as it gets new items it deletes the old items 
+
+    for i in df.values:
+        prev_days.append([n for n in i[:-1]]) # each of the columns up to the last column (target)
+        if len(prev_days) == seq_len:
+            seq_data.append([np.array(prev_days), i[-1]])
+    np.random.shuffle(seq_data)
+
+    # balance the data 
+    buys = []
+    sells = []
+    for seq, target in seq_data:
+        if target== 0:
+            sells.append([seq,target])
+        elif target ==1:
+            buys.append([seq, target])
+
+    np.random.shuffle(buys)
+    np.random.shuffle(sells)
+
+    lower = min(len(buys), len(sells))
+    buys = buys[:lower]
+    sells = sells[:lower]
+
+    seq_data = buys+sells
+    np.random.shuffle(seq_data)
+
+    X = []
+    y = []
+    
+    for seq, target in seq_data:
+        X.append(seq)
+        y.append(target)
+    
+    return(np.array(X),np.array(y))
